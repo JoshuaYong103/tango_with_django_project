0e4ced670fbee0a1bd727268174209ef295fe538
Shiping Yong <2598902y@student.gla.ac.uk>
2021-07-29 12:48:49 +0100

    general commit in CH6

--- populate_rango.py
+++ populate_rango.py
@@ -8,25 +8,25 @@ from rango.models import Category,Page
 def populate():
     python_page =[
          {'title':'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
+         'url':'http://docs.python.org/3/tutorial/','views':20},
          {'title':'How to Think like a Computer Scientist',
-         'url':'http;//www.greentearpress.com/thinkpython'},
+         'url':'http;//www.greentearpress.com/thinkpython/','views':55},
          {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorial/python/'}
+         'url':'http://www.korokithakis.net/tutorial/python/','views':30}
      ]
     django_pages =[
          {'title':'Official Django Tutorial',
-         'url':'http://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url':'http://docs.djangoproject.com/en/2.1/intro/tutorial01/','views':56},
          {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+         'url':'http://www.djangorocks.com/','views':89},
          {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'}
+         'url':'http://www.tangowithdjango.com/','views':123}
      ]
     other_pages=[
         {'title':'Bottle',
-        'url':'http://bottlepy.org/docs/dev/'},
+        'url':'http://bottlepy.org/docs/dev/','views':78},
         {'title':'Flask',
-        'url':'http://flask.pocoo.org'}
+        'url':'http://flask.pocoo.org','views':32}
      ]
     cats ={
          'Python':{'pages':python_page,'views':128,'likes':64},
