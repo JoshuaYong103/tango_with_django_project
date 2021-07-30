2c8481056ca6928944d42b79b10ef1f30cae5a91
Shiping Yong <2598902y@student.gla.ac.uk>
2021-07-30 09:50:01 +0100

    Ex in CH9 finished

--- rango/views.py
+++ rango/views.py
@@ -9,6 +9,9 @@ from django.urls import reverse
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate,login
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -35,6 +38,7 @@ def show_category(request,category_name_slug):
         context_dict['pages']=None
     return render(request,'rango/category.html',context=context_dict)
 
+@login_required
 def add_category(request):
     form=CategoryForm()
 
@@ -47,6 +51,7 @@ def add_category(request):
         print(form.errors)
 
     return render(request,'rango/add_category.html',{'form':form})
+@login_required
 def add_page(request,category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -119,4 +124,11 @@ def user_login(request):
             print(f"Invalid login details:{username},{password}")
             return HttpResponse("Invalid login details supplied.")
     else:
-        return render(request,'rango/login.html')                                   
+        return render(request,'rango/login.html')  
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!") 
+@login_required       
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))                            
