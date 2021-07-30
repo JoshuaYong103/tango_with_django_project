bb500d2d1aedcfd5e4f04a1446dc9f60c9568ba5
Shiping Yong <2598902y@student.gla.ac.uk>
2021-07-30 06:55:07 +0100

    half way of CH8 EX

--- rango/views.py
+++ rango/views.py
@@ -39,7 +39,7 @@ def add_category(request):
         form=CategoryForm(request.POST)
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect('rango:index')
     else:
         print(form.errors)
 
@@ -50,7 +50,7 @@ def add_page(request,category_name_slug):
     except Category.DoesNotExist:
         category = None
     if category is None:
-        return redirect('/rango/')
+        return redirect('rango:index')
     form = PageForm()
 
     if request.method == 'POST':
